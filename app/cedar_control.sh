f71cd76d4f3701acced06c6da4fc61889250bc61
Jason Priebe <jason.priebe@sas.com>
2020-03-31 13:32:12 -0400

    improved logging; removed yaml (now in ac-cluster)

--- app/cedar_control.sh
+++ app/cedar_control.sh
@@ -7,9 +7,14 @@ ln -s /etc/secret/.dockerconfigjson /root/.docker/config.json
 
 daytimer=0
 
+function log {
+    echo "$(date -I'seconds') - $*"
+}
+
 while true
 do
 
+log "Gathering images from manifests"
 
 # Step 1, retreive the manifests in use.  This has changed quite a bit, but this seems to be the best way for now:
 
@@ -26,25 +31,25 @@ sorted_image_array=($(echo "${image_array[@]}" | tr ' ' '\n' | sort -u | tr '\n'
 
 # Now we have our target array, we can go to work.
 
-echo Printing Current Image List:
-echo ${sorted_image_array[@]}
+log Printing Current Image List:
+log ${sorted_image_array[@]}
 
 # test for repo availability before we try and pull anything down
 /usr/bin/curl --silent --connect-timeout 3 https://registry.unx.sas.com > /dev/null
 if [ $? -eq 0 ]; then
-  echo 'Repo registry.unx.sas.com is available'
+  log 'Repo registry.unx.sas.com is available'
   REGISTRY_REACH=TRUE
 else
-  echo 'unable to connect to registry.unx.sas.com'
+  log 'unable to connect to registry.unx.sas.com'
   REGISTRY_REACH=FALSE
 fi
 
 /usr/bin/curl --silent --connect-timeout 3 https://harbor.unx.sas.com > /dev/null
 if [ $? -eq 0 ]; then
-  echo 'Repo harbor.unx.sas.com is available'
+  log 'Repo harbor.unx.sas.com is available'
   HARBOR_REACH=TRUE
 else
-  echo 'unable to connect to harbor.unx.sas.com'
+  log 'unable to connect to harbor.unx.sas.com'
   HARBOR_REACH=FALSE
 fi
 
@@ -53,23 +58,23 @@ fi
 
 if [ $REGISTRY_REACH -a $HARBOR_REACH ]; 
  then
-   echo 'Repos are Available, Beginning Control Loop'
-   echo 'Attempting Docker Pull from: harbor.unx.sas.com'
+   log 'Repos are Available, Beginning Control Loop'
+   log 'Attempting Docker Pull from: harbor.unx.sas.com'
    for i in ${sorted_image_array[@]}
    do
-    echo "Attempting Pull from $i"
+    log "Attempting Pull from $i"
     docker pull $i 
    done
   
-   echo 'Sleeping a few seconds'
+   log 'Sleeping a few seconds'
    sleep 3600
-   echo "Total Daily Run Time = $daytimer"
+   log "Total Daily Run Time = $daytimer"
    daytimer+=1 
    # Increment day timer until a day is reached.  Then, do a deep system prune
    if (daytimer -ge 20); then
-      echo "RunTimer has reached $daytimer, beginning system prune"
+      log "RunTimer has reached $daytimer, beginning system prune"
       docker system prune --filter "until=72h" --filter=label=maintainer="PDT <pdt@wnt.sas.com>"
-      echo "Prune Complete, resetting..."
+      log "Prune Complete, resetting..."
       daytimer=0
    fi
 fi
