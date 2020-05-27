fcd10b98d1613c09d970579f8729df4a72283c60
Jason Priebe <jason.priebe@sas.com>
2020-05-27 16:05:37 -0400

    made image extraction a little more generic so it will work with registry.sas.com as well as registry.unx.sas.com

--- app/cedar_control.sh
+++ app/cedar_control.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-daytimer=0
+runcount=0
 
 function log {
     echo "$(date -I'seconds') - $*"
@@ -13,12 +13,11 @@ log "Gathering images from manifests"
 
 # Step 1, retreive the manifests in use.  This has changed quite a bit, but this seems to be the best way for now:
 
-mapfile -t image_array1 < <(kubectl get rs --all-namespaces -o yaml | grep 'registry.unx.sas.com' | grep -oP '(?<=image: ).*' )
-mapfile -t image_array2 < <(kubectl get rs --all-namespaces -o yaml | grep 'docker.sas.com' | grep -oP '(?<=image: ).*' )
-mapfile -t image_array3 < <(kubectl get deployment --all-namespaces -o yaml | grep 'docker.sas.com' | grep -oP '(?<=image: ).*' )
-mapfile -t image_array4 < <(kubectl get deployment --all-namespaces -o yaml | grep 'registry.unx.sas.com' | grep -oP '(?<=image: ).*' )
+mapfile -t image_array1 < <(kubectl get rs --all-namespaces -o yaml | grep 'image:' | grep 'sas.com' )
+mapfile -t image_array2 < <(kubectl get deployment --all-namespaces -o yaml | grep 'image:' | grep 'sas.com' )
+mapfile -t image_array3 < <(kubectl get daemonset --all-namespaces -o yaml | grep 'image:' | grep 'sas.com' )
 
-image_array=( "${image_array1[@]}" "${image_array2[@]}" "${image_array3[@]}" "${image_array4[@]}" )
+image_array=( "${image_array1[@]}" "${image_array2[@]}" "${image_array3[@]}" )
 
 # Step 2, sort this highly redundant array
 
@@ -29,49 +28,23 @@ sorted_image_array=($(echo "${image_array[@]}" | tr ' ' '\n' | sort -u | tr '\n'
 log Printing Current Image List:
 log ${sorted_image_array[@]}
 
-# test for repo availability before we try and pull anything down
-/usr/bin/curl --silent --connect-timeout 3 https://registry.unx.sas.com > /dev/null
-if [ $? -eq 0 ]; then
-  log 'Repo registry.unx.sas.com is available'
-  REGISTRY_REACH=TRUE
-else
-  log 'unable to connect to registry.unx.sas.com'
-  REGISTRY_REACH=FALSE
-fi
-
-/usr/bin/curl --silent --connect-timeout 3 https://docker.sas.com > /dev/null
-if [ $? -eq 0 ]; then
-  log 'Repo docker.sas.com is available'
-  DOCKER_REACH=TRUE
-else
-  log 'unable to connect to docker.sas.com'
-  DOCKER_REACH=FALSE
-fi
-
-
-# Step 3, if the repos are up, let's go out and do our fetching fetch.  After 20 runs we'll do a system prune.
-
-if [ $REGISTRY_REACH -a $DOCKER_REACH ]; 
- then
-   log 'Repos are Available, Beginning Control Loop'
-   log 'Attempting Docker Pull from our repos...'
-   for i in ${sorted_image_array[@]}
-   do
-    log "Attempting Pull from $i"
-    docker pull $i 
-   done
+log 'Attempting Docker Pull from our repos...'
+for i in ${sorted_image_array[@]}
+do
+ log "docker pull $i"
+ docker pull $i 
+done
   
-   log 'Sleeping a few seconds'
-   sleep 3600
-   log "Total Daily Run Time = $daytimer"
-   daytimer+=1 
-   # Increment day timer until a day is reached.  Then, do a deep system prune
-   if (daytimer -ge 20); then
-      log "RunTimer has reached $daytimer, beginning system prune"
-      docker system prune --filter "until=72h" --filter=label=maintainer="PDT <pdt@wnt.sas.com>"
-      log "Prune Complete, resetting..."
-      daytimer=0
-   fi
+log 'Sleeping for one hour'
+sleep 3600
+runcount+=1 
+log "runcount = $runcount"
+# Increment day timer until a day is reached.  Then, do a deep system prune
+if (runcount -ge 20); then
+   log "runcount has reached $runcount, beginning system prune"
+   docker system prune --filter "until=72h" --filter=label=maintainer="PDT <pdt@wnt.sas.com>"
+   log "Prune Complete, resetting..."
+   runcount=0
 fi
 
 done
